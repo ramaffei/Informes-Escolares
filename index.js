22497ee9413aa9031293cf88a9ecc734287e3744
Rodrigo Maffei <rodrigoa.maffei@gmail.com>
2021-12-10 10:58:35 -0300

    cambios segunda etapa

--- index.js
+++ index.js
@@ -19,60 +19,68 @@ function validarYEjecutar(){
 }
 
 async function generarInforme(){
-      const dni = inputBuscar.value;
-      let respuesta = await fetch(`https://script.google.com/macros/s/AKfycbwx92_dBQaY46hA-QZxYIXA4C2IYCWRevBsyLkz-pCWF7nzWrI7Dnd4Rf8UGJ-lhidD/exec?dni=${dni}`)
+      const value = inputBuscar.value;
+      let respuesta = await fetch(`https://script.google.com/macros/s/AKfycbwx92_dBQaY46hA-QZxYIXA4C2IYCWRevBsyLkz-pCWF7nzWrI7Dnd4Rf8UGJ-lhidD/exec?dni=${value}`)
       respuesta = await respuesta.json();
 
-      const cuerpoTabla = document.getElementById("tablaInforme");
-      cuerpoTabla.innerHTML = ""
       const keys = Object.keys(respuesta);
       
       if (keys.length < 1){
         mensajeModal('No se encuentra en DNI especificado', 'error')
         return false
       }
-
-      const materias = respuesta[keys[0]].MATERIAS
+      const dni = keys[0]
+      const materias = respuesta[dni].MATERIAS
       document.getElementById("tituloModalInforme").innerHTML = "RESUMEN DE CALIFICACIONES"; 
-      document.getElementById("nombreEstudianteInforme").value = respuesta[keys[0]].NOMBRE;
-      document.getElementById("dniEstudianteInforme").value = keys[0];
-      document.getElementById("burbujaEstudianteInforme").value = `${respuesta[keys[0]].CURSO} - ${respuesta[keys[0]].TURNO}`; 
-  
-    await materias.forEach(async function(materia){
+      document.getElementById("nombreEstudianteInforme").value = respuesta[dni].NOMBRE;
+      document.getElementById("dniEstudianteInforme").value = dni;
+      document.getElementById("burbujaEstudianteInforme").value = `${respuesta[dni].CURSO} - ${respuesta[dni].TURNO}`; 
+      const etapas = ['ETAPA1', 'ETAPA2']
+      for (const i in etapas ){
+        const cuerpoTabla = document.getElementById(`tablaInforme-${i}`);
+        cuerpoTabla.innerHTML = ""
+        
+        const etapa = etapas[i]
 
-    const row = document.createElement("tr");
-    const col1 = document.createElement("td"); 
-    const col2 = document.createElement("td");
-    const col3 = document.createElement("td");
-    const col4 = document.createElement("td");
-    const col5 = document.createElement("td");
-    const col6 = document.createElement("td");
-    const col7 = document.createElement("td");
+          await materias.forEach(async function(materia){
 
-          
-    col1.textContent = materia;//set nombre
-
-    if(respuesta[keys[0]].hasOwnProperty(materia)){
-    col2.textContent = respuesta[keys[0]][materia]['Aprendizajes'][1];
-    col3.textContent = respuesta[keys[0]][materia]['Aprendizajes'][2];
-    col4.textContent = respuesta[keys[0]][materia]['Aprendizajes'][3];
-    col5.textContent = respuesta[keys[0]][materia]['Aprendizajes'][4];
-    col6.textContent = respuesta[keys[0]][materia]['Valoracion'];
-    col7.textContent = respuesta[keys[0]][materia]['Sumativa'];
-    }
-
-    row.appendChild(col1); 
-    row.appendChild(col2); 
-    row.appendChild(col3); 
-    row.appendChild(col4); 
-    row.appendChild(col5);
-    row.appendChild(col6); 
-    row.appendChild(col7);     
-
-    cuerpoTabla.appendChild(row);
-
-  });
+            const row = document.createElement("tr");
+            const col1 = document.createElement("td"); 
+            const col2 = document.createElement("td");
+            const col3 = document.createElement("td");
+            const col4 = document.createElement("td");
+            const col5 = document.createElement("td");
+            const col6 = document.createElement("td");
+            const col7 = document.createElement("td");
+      
+                  
+            col1.textContent = materia;//set nombre
+      
+            if(respuesta[dni]['NOTAS'][etapa].hasOwnProperty(materia)){
+              col2.textContent = respuesta[dni]['NOTAS'][etapa][materia]['Aprendizajes'][1];
+              col3.textContent = respuesta[dni]['NOTAS'][etapa][materia]['Aprendizajes'][2];
+              col4.textContent = respuesta[dni]['NOTAS'][etapa][materia]['Aprendizajes'][3];
+              col5.textContent = respuesta[dni]['NOTAS'][etapa][materia]['Aprendizajes'][4];
+              col6.textContent = respuesta[dni]['NOTAS'][etapa][materia]['Valoracion'];
+              col7.textContent = respuesta[dni]['NOTAS'][etapa][materia]['Sumativa'];
+            }
+      
+            row.appendChild(col1); 
+            row.appendChild(col2); 
+            row.appendChild(col3); 
+            row.appendChild(col4); 
+            row.appendChild(col5);
+            row.appendChild(col6); 
+            row.appendChild(col7);     
+      
+            cuerpoTabla.appendChild(row);
+      
+        });
+      }
+      
+    
             M.updateTextFields();
             divRespuesta.classList.remove('hide')
+            M.Tabs.init(document.getElementById('contTabs'));
             cerrarModal();
 }
